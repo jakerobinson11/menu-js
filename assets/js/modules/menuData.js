ebc356c170b36553a1f6a6b76a542ac8efe2bd04
jakerobinson11 <jake.robinson11@gmail.com>
2024-10-15 16:23:10 +0200

    updated menu

--- assets/js/modules/menuData.js
+++ assets/js/modules/menuData.js
@@ -41,3 +41,4 @@ const menuData = [
         ]
     },
 ]
+export {menuData};
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,60 @@
+
+import { menuData } from "./modules/menuData.js";
+console.dir(menuData);
+
+
 //VARIABLES
 const navHeaderUl = document.querySelector("#header-nav>ul");
-const menuPrincipale = [
-    "Item1",
-    "Item2",
-    "Item3",
-    "Item4",
-    "Item5"
-];
-const sousMenu = [
-    [
-        "Sous Item 1",
-        "Sous Item 2",
-        "Sous Item 3"
-    ],
-    [
-        "Sous Item 1",
-        "Sous Item 2",
-        "Sous Item 3",
-        "Sous Item 4"
-    ],
-    [
-        "Sous Item 1",
-        "Sous Item 2",
-        "Sous Item 3"
-    ],
-    [
-        "Sous Item 1",
-        "Sous Item 2",
-        "Sous Item 3"
-    ],
-    [
-        "Sous Item 1"
-    ]
-]
+//const menuItems = document.createElement("li");
 
-// FUNCTIONS
-function ouvertureMenu(itemNumber) {
+//FUNCTIONS
+/* function appendItems (i) {
+    navHeaderUl.append(menuItems);
+    menuItems.innerHTML = menuData[i].title;
+    };
+appendItems(0); */
 
-    // ajouter un evenement click sur un element avec un id "item0"
-    // pour qu'il envoie un console log bonjour
-    document.querySelector("#item" + itemNumber).addEventListener("click", function () {
-        // je supprime avec une boucle tous les sous menu ouverts (not undefined)
-        if (document.body.contains(document.querySelector(`#item${itemNumber} ul`))) {
-            document.querySelector(`#item${itemNumber} ul`).remove();
-        } else {
-            for (let i = 0; i < sousMenu.length; i++) {
-                if (document.querySelector(`#item${i} ul`) != undefined) {
-                    document.querySelector(`#item${i} ul`).remove();
-                }
+/* function appendItems([i]){
+    navHeaderUl.append(menuItems)
+    menuItems.innerHTML = menuData[i].title;
+    };
+appendItems([0]); */
+
+let activeSubMenu = null;  // Track the currently visible submenu
+let activeListItem = null; // Track the currently clicked list item
+
+menuData.forEach((menuItem) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = menuItem.title; // Set the title as the li text
+    navHeaderUl.appendChild(listItem); // Append the li to the ul
+
+    // Event listener for each list item
+    listItem.addEventListener("click", () => {
+         // If the same item is clicked and the submenu is active, remove it
+         if (activeListItem === listItem) {
+            if (activeSubMenu) {
+                activeSubMenu.remove();
+                activeSubMenu = null;
+                activeListItem = null; // Reset active list item
             }
-            const ulSubMenu = document.createElement("ul");
-            //insertion de cet element dans un parent AVANT son contenu
-            // document.querySelector("#item0").prepend(ulSubMenu);
-            //insertion de cet element dans un parent APRES son contenu
-            document.querySelector("#item" + itemNumber).append(ulSubMenu);
-            sousMenu[itemNumber].forEach(function (element, index) {
-                console.log(element, index);
-                const liSubMenu = document.createElement("li");
-                liSubMenu.innerText = element;
-                ulSubMenu.append(liSubMenu);
-            });
-        } 
-    })
-}
+            return;
+        }
+        // Hide the currently active submenu if there is one
+        if (activeSubMenu) {
+            activeSubMenu.remove();
+        }
 
+        // Create the new submenu (ul) and append it after the clicked item
+        const subMenuUl = document.createElement("ul");
+        subMenuUl.classList.add("submenu"); // Add a class for styling if needed
+        menuItem.sousMenu.forEach(subMenuItem => {
+            const subMenuLi = document.createElement("li");
+            subMenuLi.textContent = subMenuItem;
+            subMenuUl.append(subMenuLi);
+        });
 
-// LOGIQUE
-let i = 0;
-
-while (i < menuPrincipale.length) {
-    let n = i;
-    /* navHeaderUl.innerHTML += "<li id='item'>" +menuPrincipale[i]+"</li>"; */
-    navHeaderUl.innerHTML += `<li id="item${i}">${menuPrincipale[i]}</li>`;
-    i++;
-}
-ouvertureMenu(0);
-ouvertureMenu(1);
-ouvertureMenu(2);
-ouvertureMenu(3);
-ouvertureMenu(4);
+        listItem.append(subMenuUl);  // Append the submenu to the clicked list item
+        activeSubMenu = subMenuUl;  // Set this submenu as active
+        activeListItem = listItem;  // Set this list item as the currently active one
+    });
+});
