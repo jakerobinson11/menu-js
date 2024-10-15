d694b91038bd609314a4fd4b6adfdfb86dbfdb69
jakerobinson11 <jake.robinson11@gmail.com>
2024-10-15 14:40:41 +0200

    added menuData

---
+++ assets/js/modules/menuData.js
@@ -0,0 +1,43 @@
+const menuData = [
+    {
+        title: "Item1",
+        sousMenu: [
+            "Item1",
+            "Item2",
+            "Item3",
+            "Item4",
+            "Item5"
+        ]
+    },
+    {
+        title: "Item2",
+        sousMenu: [
+            "Item1",
+            "Item2",
+            "Item3",
+            "Item4"
+        ]
+    },{
+        title: "Item3",
+        sousMenu: [
+            "Item1",
+            "Item2",
+            "Item3"
+        ]
+    },{
+        title: "Item4",
+        sousMenu: [
+            "Item1",
+            "Item2",
+            "Item3",
+            "Item4",
+            "Item5"
+        ]
+    },{
+        title: "Item5",
+        sousMenu: [
+            "Item1",
+            "Item2"
+        ]
+    },
+]
